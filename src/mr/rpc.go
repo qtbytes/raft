6a62b0db06b7818f7d7bdc1d640a9d4fe7392513
qt <qtbytes@outlook.com>
2025-03-04 21:35:11 +0800

    Define RPC args and replys

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -6,8 +6,10 @@ package mr
 // remember to capitalize all names.
 //
 
-import "os"
-import "strconv"
+import (
+	"os"
+	"strconv"
+)
 
 //
 // example to show how to declare the arguments
@@ -15,15 +17,17 @@ import "strconv"
 //
 
 type ExampleArgs struct {
-	X int
+	query_id int
+	task_id  int
 }
 
 type ExampleReply struct {
-	Y int
+	task_id int
+	task    string
+	nReduce int
 }
 
 // Add your RPC definitions here.
-
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
