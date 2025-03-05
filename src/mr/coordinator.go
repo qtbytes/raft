3d7ac16fce921210c62ef6c9d1e5ff0e128fd333
qt <qtbytes@outlook.com>
2025-03-05 20:44:10 +0800

    Use lock and fix reply to workers

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,62 +1,84 @@
 package mr
 
 import (
+	"fmt"
 	"log"
 	"net"
 	"net/http"
 	"net/rpc"
 	"os"
+	"sync"
 )
 
-// const UN_STARTED = 0
-// const MAP_FINISHED = 1
-// const REDUCE_FINISHED = 2
+// task state
+const UN_STARTED = 0
+const STARTED = 1
+const FINISHED = 2
+
+// workers grpc args
 const ASK_MAP = 3
-const FINISH_MAP = 4
-const FINISH_REDUCE = 5
+const ASK_REDUCE = 4
+const FINISH = 5
+
+// coordinator grpc replys
+const MAP_TASK = 6
+const REDUCE_TASK = 7
+
+const TIMEOUT = 10
 
 type Coordinator struct {
+	lock         sync.Mutex
+	nMap         int
 	nReduce      int
 	tasks        []string
-	mapStates    []bool
+	mapStates    []int
 	mapCount     int
-	reduceStates []bool
+	reduceStates []int
 	reduceCount  int
 }
 
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
+	c.lock.Lock()
+	defer c.lock.Unlock()
 
 	// check worker's request
-	if args.query_id == FINISH_REDUCE {
-		c.reduceStates[args.task_id] = true
+	if args.QueryID == FINISH {
+		c.reduceStates[args.TaskID] = FINISHED
 		c.reduceCount--
-	} else if args.query_id == FINISH_MAP {
-		c.mapStates[args.task_id] = true
+	} else if args.QueryID == ASK_REDUCE {
+		c.mapStates[args.TaskID] = FINISHED
 		c.mapCount--
 	}
 
 	// send tasks to worker
 	if c.mapCount > 0 {
 		// Send map task to workers
-		for i, finish := range c.mapStates {
-			if !finish {
-				reply.task_id = i
-				reply.task = c.tasks[i]
-				reply.nReduce = c.nReduce
+		for i, state := range c.mapStates {
+			if state == UN_STARTED {
+				c.mapStates[i] = STARTED
+				reply.TaskType = MAP_TASK
+				reply.TaskID = i
+				reply.Task = c.tasks[i]
+				reply.NReduce = c.nReduce
+				fmt.Println("Map task states", c.mapStates)
 				break
 			}
 		}
 
 	} else if c.reduceCount > 0 {
-		// Send map task to workers
-		for i, finish := range c.reduceStates {
-			if !finish {
-				reply.task_id = i
-				reply.task = ""
-				reply.nReduce = c.nReduce
+		// Send reduce task to workers
+		for i, state := range c.reduceStates {
+			if state == UN_STARTED {
+				c.reduceStates[i] = STARTED
+				reply.TaskType = REDUCE_TASK
+				reply.TaskID = i
+				reply.NMap = c.nMap
+				fmt.Println("Reduce task states", c.reduceStates)
 				break
 			}
 		}
+	} else {
+		reply.TaskType = FINISH
 	}
 	return nil
 }
@@ -86,13 +108,14 @@ func (c *Coordinator) Done() bool {
 // nReduce is the number of reduce tasks to use.
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
-
+	c.lock = sync.Mutex{}
+	c.nMap = len(files)
 	c.nReduce = nReduce
 	c.tasks = files
-	c.mapCount = len(files)
-	c.mapStates = make([]bool, len(files))
+	c.mapCount = c.nMap
+	c.mapStates = make([]int, c.nMap)
 	c.reduceCount = nReduce
-	c.reduceStates = make([]bool, nReduce)
+	c.reduceStates = make([]int, nReduce)
 
 	c.server()
 	return &c
